944c9c726fa0fe5816de20ca2d221eb9913702bd
Joshua Tucker <thejltucker@gmail.com>
2019-04-01 12:31:26 -0400

    Added Book Edit and Delete Controller Methods

--- BookList/Controllers/BooksController.cs
+++ BookList/Controllers/BooksController.cs
@@ -54,6 +54,68 @@ namespace BookList.Controllers
             return View(book);
         }
 
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.ID == id);
+
+            if (book == null)
+                return NotFound();
+
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Book book)
+        {
+            if (id != book.ID)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _db.Update(book);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.ID == id);
+
+            if (book == null)
+                return NotFound();
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.ID == id);
+            if (book != null)
+            {
+                _db.Remove(book);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return BadRequest();
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
